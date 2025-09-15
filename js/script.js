3585375280649a913063e5fbda5390ddea34575d
ShriiiTrackingSolutions <shriitrackingsolutions86@gmail.com>
2025-09-15 17:45:10 +0530

    trips and Camp page completed

--- js/script.js
+++ js/script.js
@@ -58,23 +58,10 @@ function loadHeaderFooter(callback) {
                   <li class="navLi">
                     <a href="activities.html" class="navLink">Activities</a>
                   </li>
-                  
                   <li class="navLi">
-                    <a href="#" class="navLink">Trips And Camps <span class="toggleSub">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16">
-                          <path fill="currentColor" fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .75.75v4.75h4.75a.75.75 0 0 1 0 1.5H8.75v4.75a.75.75 0 0 1-1.5 0V8.75H2.5a.75.75 0 0 1 0-1.5h4.75V2.5A.75.75 0 0 1 8 1.75" clip-rule="evenodd" />
-                        </svg>
-                      </span>
-                    </a>
-                    <ul class="dropdownList">
-                      <li class="navLi">
-                        <a href="#" class="navLink">Links</a>
-                      </li>
-                      <li class="navLi">
-                        <a href="#" class="navLink">Links</a>
-                      </li>
-                    </ul>
+                    <a href="tripsAndCamps.html" class="navLink">Trips And Camps</a>
                   </li>
+                  
                   <li class="navLi">
                     <a href="#" class="navLink">Gallery</a>
                   </li>
